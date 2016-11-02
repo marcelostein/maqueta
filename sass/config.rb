f5ca6094a7325525f9a181655717487a67039c7c
Uriel Stein <mstein@edespegar.com>
2016-11-02 18:36:18 -0300

    Maqueta Nirmala

---
+++ sass/config.rb
@@ -0,0 +1,7 @@
+#config.rb
+
+
+css_dir = "css" # Carpeta en donde iran los .css
+sass_dir = "sass" # Carpeta en donde iran los .scss
+images_dir = "images" # Carpeta en donde van las imagenes
+output_style = :expanded # :expanded, :nested, :compact, :compressed
